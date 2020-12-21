6db30eef141be7cb3452da6394ee7e8ebe1f0470
root <root@cfb31b7ede07>
2020-12-21 21:29:20 +0000

    Roller Coaster

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -6,20 +6,42 @@ public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
 		
+	public static void main(String[] args) {
+	
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
 		
-		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
+	String heightToRide = JOptionPane.showInputDialog("Enter your height in inches.");
 		
-		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
+		// Uncomment the line below...
+	
+	int heightAsInt = Integer.parseInt(heightToRide);
+	
+	if(heightAsInt >= 48) { 
+		
+		// Do you see the heightToRide > 48 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
 		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
 	
 		// 4) If the heightAsInt is greater than or equal to 48 inches tall print:
 	    // "Hurray! You are tall enough to ride the coaster alone!" Hint: use JoptionPane
-		
+	
+	JOptionPane.showMessageDialog(null, "Nice! You're tall enough to ride alone!");
+	}
 		// 5) Else if the heightAsInt is greater than or equal to 42 inches tall print:
 		// "You can ride the coaster with someone else!" 
 	
+	else if (heightAsInt >= 42) {
+		
+		JOptionPane.showMessageDialog(null, "You can ride the coaster with someone else!");
+		
+	}
+	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-} 
+	
+	else {
+	JOptionPane.showMessageDialog(null, "You must be at least 42 inches tall to ride the roller coaster... Sorry!");
+	}
+	
+	} 
+}
+	
